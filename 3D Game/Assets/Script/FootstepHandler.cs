359bfb98090a96ded51a6ca6750a6bb15bba0e09
Sophia Riley <riley.e.sophia@gmail.com>
2023-12-10 18:43:47 -0500

    added sfx

--- 3D Game/Assets/Script/FootstepHandler.cs
+++ 3D Game/Assets/Script/FootstepHandler.cs
@@ -8,31 +8,49 @@ public class FootstepHandler : MonoBehaviour
 
     void Update()
     {
+        // Whenever the player moves (using W, A, S, or D), the footstep sound should play
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         {
+            // If the player is sprinting (using left shift), the sprinting sound should play instead
             if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
             {
                 footstepsSound.enabled = false;
-                jumpSound.enabled = false;
                 sprintSound.enabled = true;
+                jumpSound.enabled = false;
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    jumpSound.enabled = true;
+                    footstepsSound.enabled = false;
+                    sprintSound.enabled = false;
+                }
             }
             else if (Input.GetKey(KeyCode.Space))
             {
-                footstepsSound.enabled = false;
                 jumpSound.enabled = true;
+                footstepsSound.enabled = false;
                 sprintSound.enabled = false;
             }
             else
             {
                 footstepsSound.enabled = true;
-                jumpSound.enabled = false;
                 sprintSound.enabled = false;
+                jumpSound.enabled = false;
             }
         }
+
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            jumpSound.enabled = true;
+            footstepsSound.enabled = false;
+            sprintSound.enabled = false;
+        }
+
+        // If the player isn't moving, no sound should be played
         else
         {
             footstepsSound.enabled = false;
             sprintSound.enabled = false;
+            jumpSound.enabled = false;
         }
     }
 
