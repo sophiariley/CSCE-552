4f882c2167570067b9c3ae59d7a29ebaa139f12a
Georphoe <tzechen@email.sc.edu>
2023-12-11 12:25:31 -0500

    addinng all ducklings data

--- 3D Game/Assets/Script/Player.cs
+++ 3D Game/Assets/Script/Player.cs
@@ -6,17 +6,22 @@ public class Player : MonoBehaviour
 {
     public CharacterController charController;
     public GameObject hedgeDuckling;
+    public GameObject hidingDuckling;
+    public GameObject treeDuckling;
+    public GameObject benchDuckling;
     public void SavePlayer()
     {
         SaveSystem.SavePlayer(this);
-        Debug.Log("SavePlayer()");
+        //Debug.Log("SavePlayer()");
     }
 
     public void LoadPlayer()
     {
-        Debug.Log("LoadPlayer()");
+        //Debug.Log("LoadPlayer()");
+
         PlayerData data = SaveSystem.LoadPlayer();
 
+        // Load player data
         Vector3 new_position;
         new_position.x = data.position[0];
         new_position.y = data.position[1];
@@ -27,10 +32,11 @@ public class Player : MonoBehaviour
         charController.transform.position = new_position;
         charController.enabled = true;
 
-        Debug.Log("player: loadPlayer(): new x = " + data.position[0]);
-        Debug.Log("player: loadPlayer(): new y = " + data.position[1]);
-        Debug.Log("player: loadPlayer(): new z = " + data.position[2]);
+        // Debug.Log("player: loadPlayer(): new x = " + data.position[0]);
+        // Debug.Log("player: loadPlayer(): new y = " + data.position[1]);
+        // Debug.Log("player: loadPlayer(): new z = " + data.position[2]);
 
+        // Load hedge Duckling data
         Vector3 new_hedgeDuckling_position;
  
         new_hedgeDuckling_position.x = data.hedgeDucklingPosition[0];
@@ -40,6 +46,41 @@ public class Player : MonoBehaviour
 
         bool new_hedgeDucklingFollow = data.hedgeDucklingFollow;
         hedgeDuckling.GetComponent<NavigationScript>()._isFollowing = new_hedgeDucklingFollow;
+
+        // Load hiding Duckling data
+        Vector3 new_hidingDuckling_position;
+ 
+        new_hidingDuckling_position.x = data.hidingDucklingPosition[0];
+        new_hidingDuckling_position.y = data.hidingDucklingPosition[1];
+        new_hidingDuckling_position.z = data.hidingDucklingPosition[2];
+        hidingDuckling.transform.position = new_hidingDuckling_position;
+
+        bool new_hidingDucklingFollow = data.hidingDucklingFollow;
+        hidingDuckling.GetComponent<NavigationScript>()._isFollowing = new_hidingDucklingFollow;
+
+        // Load tree Duckling data
+        Vector3 new_treeDuckling_position;
+ 
+        new_treeDuckling_position.x = data.treeDucklingPosition[0];
+        new_treeDuckling_position.y = data.treeDucklingPosition[1];
+        new_treeDuckling_position.z = data.treeDucklingPosition[2];
+        treeDuckling.transform.position = new_treeDuckling_position;
+
+        bool new_treeDucklingFollow = data.treeDucklingFollow;
+        treeDuckling.GetComponent<NavigationScript>()._isFollowing = new_treeDucklingFollow;
+
+        // Load bench Duckling data
+        Vector3 new_benchDuckling_position;
+ 
+        new_benchDuckling_position.x = data.benchDucklingPosition[0];
+        new_benchDuckling_position.y = data.benchDucklingPosition[1];
+        new_benchDuckling_position.z = data.benchDucklingPosition[2];
+        benchDuckling.transform.position = new_benchDuckling_position;
+
+        bool new_benchDucklingFollow = data.benchDucklingFollow;
+        benchDuckling.GetComponent<NavigationScript>()._isFollowing = new_benchDucklingFollow;
+
+        
         
 
         
--- 3D Game/Assets/Script/PlayerData.cs
+++ 3D Game/Assets/Script/PlayerData.cs
@@ -8,20 +8,51 @@ public class PlayerData
     public float[] position; // Vector3 position;
     public float[] hedgeDucklingPosition;
     public bool hedgeDucklingFollow;
+    public float[] hidingDucklingPosition;
+    public bool hidingDucklingFollow;
+    public float[] treeDucklingPosition;
+    public bool treeDucklingFollow;
+    public float[] benchDucklingPosition;
+    public bool benchDucklingFollow;
     
 
     public PlayerData(Player player)
     {
+        // player data
         position = new float[3];
         position[0] = player.transform.position.x;
         position[1] = player.transform.position.y;
         position[2] = player.transform.position.z;
 
+        // hedge Duckling data
         hedgeDucklingPosition = new float[3];
         hedgeDucklingPosition[0] = player.hedgeDuckling.transform.position.x;
         hedgeDucklingPosition[1] = player.hedgeDuckling.transform.position.y;
         hedgeDucklingPosition[2] = player.hedgeDuckling.transform.position.z;
         hedgeDucklingFollow = player.hedgeDuckling.GetComponent<NavigationScript>()._isFollowing;
+
+        // hiding Duckling data
+        hidingDucklingPosition = new float[3];
+        hidingDucklingPosition[0] = player.hidingDuckling.transform.position.x;
+        hidingDucklingPosition[1] = player.hidingDuckling.transform.position.y;
+        hidingDucklingPosition[2] = player.hidingDuckling.transform.position.z;
+        hidingDucklingFollow = player.hidingDuckling.GetComponent<NavigationScript>()._isFollowing;
+
+        // tree Duckling data
+        treeDucklingPosition = new float[3];
+        treeDucklingPosition[0] = player.treeDuckling.transform.position.x;
+        treeDucklingPosition[1] = player.treeDuckling.transform.position.y;
+        treeDucklingPosition[2] = player.treeDuckling.transform.position.z;
+        treeDucklingFollow = player.treeDuckling.GetComponent<NavigationScript>()._isFollowing;
+
+        // bench Duckling data
+        benchDucklingPosition = new float[3];
+        benchDucklingPosition[0] = player.benchDuckling.transform.position.x;
+        benchDucklingPosition[1] = player.benchDuckling.transform.position.y;
+        benchDucklingPosition[2] = player.benchDuckling.transform.position.z;
+        benchDucklingFollow = player.benchDuckling.GetComponent<NavigationScript>()._isFollowing;
+
+        
     }
 
 
