0ccc24b58de9eceb62d174b4f777a577dca84d9a
Georphoe <tzechen@email.sc.edu>
2023-12-11 18:24:43 -0500

    add continue buttom and logic on title scene

---
+++ 3D Game/Assets/Script/SecondSceneScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SecondSceneScript : MonoBehaviour
+{
+    // Your function in the second scene
+    public void YourFunctionInSecondScene()
+    {
+        Debug.Log("Function called in the second scene!");
+    }
+}
--- 3D Game/Assets/Script/TitleScreen.cs
+++ 3D Game/Assets/Script/TitleScreen.cs
@@ -27,6 +27,48 @@ public class TitleScreen : MonoBehaviour
         SceneManager.LoadScene(gameScene);
     }
 
+    public void continueGame()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.LoadScene(gameScene);
+
+               
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == gameScene)
+        {
+            // Now the scene is fully loaded
+
+            // Find the GameObject with the SecondSceneScript component in the loaded scene
+            GameObject secondSceneObject = GameObject.FindWithTag("Player");
+
+            if (secondSceneObject != null)
+            {
+                // Get the SecondSceneScript component
+                Player secondSceneScript = secondSceneObject.GetComponent<Player>();
+
+                if (secondSceneScript != null)
+                {
+                    // Call the function in SecondSceneScript
+                    secondSceneScript.LoadPlayer();
+                }
+                else
+                {
+                    Debug.LogError("SecondSceneScript component not found on the GameObject in the second scene.");
+                }
+            }
+            else
+            {
+                Debug.LogError("GameObject with the name 'SecondSceneObject' not found in the second scene.");
+            }
+        }
+    }
+
+        
+    
+
     public void quitGame()
     {
         Application.Quit();
